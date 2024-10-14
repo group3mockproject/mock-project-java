b8295e943caeb7d59138267da2c8a28e5d3623d8
group3mockproject <groupthreemockproject@gmail.com>
2024-10-14 11:09:35 +0700

    add folder code

---
+++ javaGroup3/src/test/java/com/mockproject/javaGroup3/JavaGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockproject.javaGroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
